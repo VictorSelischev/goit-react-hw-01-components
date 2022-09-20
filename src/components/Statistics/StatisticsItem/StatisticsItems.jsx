11fab9a8e3075a4cf6a80c5da3f6503fce768bf5
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-09-20 23:26:29 +0300

    inline-style для random color

--- src/components/Statistics/StatisticsItem/StatisticsItems.jsx
+++ src/components/Statistics/StatisticsItem/StatisticsItems.jsx
@@ -1,10 +1,12 @@
+import css from './StatisticsItems.module.css';
+
 export const StatisticsItems = ({ stats }) => {
   return (
-    <ul class="stat-list">
+    <ul className={css.stat_list}>
       {stats.map(({ id, label, percentage }) => (
-        <li key={ id } className="item">
-          <span className="label">{ label }</span>
-          <span className="percentage">{ percentage }%</span>
+        <li key={ id } className={css.item} style={{backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`}}>
+          <span className={css.label}>{ label }</span>
+          <span className={css.percentage}>{ percentage }%</span>
         </li>
       ))}
     </ul>
