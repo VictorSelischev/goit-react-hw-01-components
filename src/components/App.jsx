001d73d5e761a08067b13111d5914e78d113dcd0
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-09-20 15:48:58 +0300

    Прописывает props в компоненте Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,13 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,35 @@
-export const Profile = () => {
+export const Profile = props => {
+  const {
+    username = 'Petra Marica',
+    tag = '@pmarica',
+    location = 'Salvador, Brasil',
+    avatar = 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+  } = props;
+  const { followers = 1000, views = 2000, likes = 3000 } = props.stats;
+
+//   console.log(username, tag, location, avatar, followers, views, likes);
+
   return (
     <div class="profile">
       <div class="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <img src={avatar} alt={username} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{"@"+tag}</p>
+        <p class="location">{location}</p>
       </div>
 
       <ul class="stats">
         <li>
           <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span class="quantity">{followers}</span>
         </li>
         <li>
           <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span class="quantity">{views}</span>
         </li>
         <li>
           <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span class="quantity">{likes}</span>
         </li>
       </ul>
     </div>
