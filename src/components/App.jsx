469791e04c51d825952d810b41e7f13fa8b77d91
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-09-20 19:49:42 +0300

    Оформляет стили компонента Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,18 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
+        paddingTop: 88,
+        paddingBottom: 88,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        backgroundColor: '#e7ecf2',
       }}
     >
-      <Profile
+      <Profile className = "section"
         username={user.username}
         tag={user.tag}
         location={user.location}
