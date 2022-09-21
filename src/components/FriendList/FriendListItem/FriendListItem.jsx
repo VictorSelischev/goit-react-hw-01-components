a2a0cee3cf7393d6bf8ebfd0a0843ffcdaf4bf56
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-09-21 17:46:34 +0300

    Вычисляет стиль isOnline

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,14 @@
-import css from './FriendListItem.module.css'
+import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ id, avatar, name, isOnline }) => {
   return (
-    <li key={ id } className={css.item}>
-      <span className={css.status} ></span>
-      <img className={css.avatar} src={ avatar } alt={ name } width="48" />
-          <p className={css.name}>{ name }</p>
+    <li key={id} className={css.item}>
+      <span
+        className={css.status}
+        style={{ backgroundColor: `#${isOnline ? '00ff00' : 'ed1c24'}` }}
+      ></span>
+      <img className={css.avatar} src={avatar} alt={name} width="48" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 };
