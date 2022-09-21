234d475af03742c11fa0a3af6655f46686f54305
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-09-21 18:18:22 +0300

    Создает компонент TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList'
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -30,7 +32,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={ friends } />
+      <FriendList friends={friends} />
+      <TransactionHistory items={ transactions } />
     </div>
   );
 };
